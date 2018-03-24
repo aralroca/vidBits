1ec6d349c7c0738eb47042a0e59dce46f252b365
Aral Roca Gomez <aral-rg@hotmail.com>
2018-03-24 12:11:04 +0100

    Init project

---
+++ models/video.js
@@ -0,0 +1,10 @@
+const {mongoose} = require('../database');
+
+const Video = mongoose.model(
+  'Video',
+  mongoose.Schema({
+    
+  })
+);
+
+module.exports = Video;
---
+++ test/models/video-test.js
@@ -0,0 +1,16 @@
+const {assert} = require('chai');
+const {mongoose, databaseUrl, options} = require('../../database');
+
+async function connectDatabase() {
+  await mongoose.connect(databaseUrl, options);
+  await mongoose.connection.db.dropDatabase();
+}
+
+async function disconnectDatabase() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  connectDatabase,
+  disconnectDatabase,
+}
